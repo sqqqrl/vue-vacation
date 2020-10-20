6fb65530346283bff34ef0e91cda873150793edb
Kostya <kostya738961@gmail.com>
2020-10-20 12:13:31 +0300

    Added condition to show error if user is not admin

--- client/src/views/Users.vue
+++ client/src/views/Users.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="container">
-    <div>
+    <div v-if="!error">
       <ul>
         <li v-for="(user, i) in users" :key="i">{{user.username}}</li>
       </ul>
     </div>
+    <span v-else>{{error.message}}</span>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      error: null
+    }
+  },
+
   asyncComputed: {
     users: function() {
-      return this.$store.dispatch('getAllUsers');
+      return this.$store.dispatch('getAllUsers')
+        .catch(err => this.error = err.response.data)
     }
   }
 };
