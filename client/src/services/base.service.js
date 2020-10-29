3c4bbfc524ae09efe2989e724228c31c7ed162a6
Kostya <kostya738961@gmail.com>
2020-10-29 13:32:01 +0200

    created user service

---
+++ client/src/services/base.service.js
@@ -0,0 +1,24 @@
+import qs from 'qs'
+
+import { Http } from './http.init'
+import { ResponseWrapper, ErrorWrapper } from './util'
+
+export class BaseService {
+  static request (status = { auth: false }) {
+    return new Http(status)
+  }
+
+  static responseWrapper (...rest) {
+    return new ResponseWrapper(...rest)
+  }
+
+  static errorWrapper (...rest) {
+    return new ErrorWrapper(...rest)
+  }
+
+  static querystring (obj) {
+    return qs.stringify(obj, {
+      encode: false
+    })
+  }
+}
