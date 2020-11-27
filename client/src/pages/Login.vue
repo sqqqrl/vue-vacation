212116e04d5b173ab5bd707705660b6a68b3af53
Kostya <kostya738961@gmail.com>
2020-11-27 11:42:32 +0200

    styling the login page

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="container">
-    <div class="col-4">
-      <label for="email">
-        Email:
-      </label>
-      <input v-model="email" type="email" name="email" />
-      <label for="password">
-        Password:
-      </label>
-      <input v-model="password" type="password" @keyup.enter="makeLogin" />
-      <p v-if="error">
-        {{ error }}
-      </p>
-      <button @click="makeLogin">
-        Login
-      </button>
+  <div class="content">
+    <div class="md-layout md-alignment-center">
+      <div class="md-layout-item md-medium-size-100 md-size-50">
+        <md-card>
+          <md-card-header :data-background-color="dataBackgroundColor">
+            <h4 class="title">Login</h4>
+          </md-card-header>
+
+          <md-card-content>
+            <div class="md-layout">
+              <div class="md-layout-item md-small-size-100 md-size-50">
+                <md-field>
+                  <label>Email Address</label>
+                  <md-input v-model="email" type="email"></md-input>
+                </md-field>
+              </div>
+              <div class="md-layout-item md-small-size-100 md-size-50">
+                <md-field>
+                  <label>Password</label>
+                  <md-input v-model="password" type="text"></md-input>
+                </md-field>
+              </div>
+              <div class="md-layout-item md-size-100 text-center">
+                <md-button class="md-raised md-success" @click="makeLogin">Login</md-button>
+              </div>
+            </div>
+          </md-card-content>
+        </md-card>
+      </div>
     </div>
   </div>
 </template>
@@ -26,6 +39,7 @@ export default {
   name: "LoginUser",
   data() {
     return {
+      dataBackgroundColor: "green",
       email: "mkpmkp@drydr.kigh",
       password: "123",
       error: ""
