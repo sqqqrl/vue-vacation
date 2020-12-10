76caa439e0fab820bf008f9ae8cb87b78d84b911
Kostya <33701606+sqqqrl@users.noreply.github.com>
2020-12-10 17:46:42 +0200

    init from validation (front)

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,72 +1,143 @@
 <template>
   <div class="content">
-    <div class="md-layout md-alignment-center">
-      <div class="md-layout-item md-medium-size-100 md-size-50">
-        <md-card>
-          <md-card-header :data-background-color="dataBackgroundColor">
-            <h4 class="title">Login</h4>
-          </md-card-header>
+    <form novalidate @submit.prevent="validateUser">
+      <div class="md-layout md-alignment-center">
+        <div class="md-layout-item md-medium-size-100 md-size-50">
+          <md-card>
+            <md-card-header :data-background-color="dataBackgroundColor">
+              <h4 class="title">Login</h4>
+            </md-card-header>
 
-          <md-card-content>
-            <div class="md-layout">
-              <div class="md-layout-item md-small-size-100 md-size-50">
-                <md-field>
-                  <label>Email Address</label>
-                  <md-input v-model="email" type="email"></md-input>
-                </md-field>
+            <md-card-content>
+              <div class="md-layout">
+                <div class="md-layout-item md-small-size-100 md-size-50">
+                  <md-field :class="getValidationClass('email')">
+                    <label for="email">Email Address</label>
+                    <md-input
+                      type="email"
+                      name="email"
+                      id="email"
+                      v-model="form.email"
+                      :disabled="sending"
+                    ></md-input>
+                    <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+                    <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-small-size-100 md-size-50">
+                  <md-field :class="getValidationClass('password')">
+                    <label>Password</label>
+                    <md-input
+                      type="password"
+                      name="password"
+                      id="password"
+                      v-model="form.password"
+                      :disabled="sending"
+                    ></md-input>
+                    <span class="md-error" v-if="!$v.form.password.required">The password is required</span>
+                    <span class="md-error" v-else-if="!$v.form.password.minLength">Invalid password</span>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-size-100 text-center">
+                  <span class="text-error">{{ error }}</span>
+                </div>
+                <div class="md-layout-item md-size-100 text-center">
+                  <md-button
+                    type="submit"
+                    class="md-raised md-success"
+                    :disabled="sending"
+                  >
+                    Login
+                  </md-button>
+                </div>
               </div>
-              <div class="md-layout-item md-small-size-100 md-size-50">
-                <md-field>
-                  <label>Password</label>
-                  <md-input v-model="password" type="password"></md-input>
-                </md-field>
-              </div>
-              <div class="md-layout-item md-size-100 text-center">
-                <span class="text-error">{{ error }}</span>
-              </div>
-              <div class="md-layout-item md-size-100 text-center">
-                <md-button class="md-raised md-success" @click="makeLogin"
-                  >Login</md-button
-                >
-              </div>
-            </div>
-          </md-card-content>
-        </md-card>
+            </md-card-content>
+          </md-card>
+        </div>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <script>
 import { AuthService } from "@/services/auth.service";
+import { validationMixin } from 'vuelidate';
+import {
+  required,
+  email,
+  minLength,
+  // maxLength
+} from 'vuelidate/lib/validators';
 
 export default {
   name: "LoginUser",
+  mixins: [validationMixin],
   data() {
     return {
       dataBackgroundColor: "green",
-      email: "mkpmkp@drydr.kigh",
-      password: "123",
+      form: {
+        email: "",
+        password: "",
+      },
+      sending: false,
       error: ""
     };
   },
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(8)
+      }
+    }
+  },
   methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
+
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
+        }
+      }
+    },
+
     async makeLogin() {
       try {
+        this.sending = true;
         await AuthService.makeLogin({
-          email: this.email,
-          password: this.password
+          email: this.form.email,
+          password: this.form.password
         });
         this.error = "";
         await this.$store.dispatch("user/getCurrent");
         await this.$router.push("dashboard");
       } catch (error) {
+        this.sending = false;
         this.error =
           error.status === 404
             ? "User with same email not found"
             : error.message;
       }
+    },
+
+    validateUser () {
+      this.$v.$touch()
+
+      if (!this.$v.$invalid) {
+        this.makeLogin()
+      }
     }
   }
 };
 </script>
+
+<style scoped>
+.md-field.md-theme-default.md-invalid label {
+  color: #ff1744 !important;
+}
+</style>
--- client/src/pages/Register.vue
+++ client/src/pages/Register.vue
@@ -1,71 +1,138 @@
 <template>
   <div class="content">
-    <div class="md-layout md-alignment-center">
-      <div class="md-layout-item md-medium-size-100 md-size-50">
-        <md-card>
-          <md-card-header :data-background-color="dataBackgroundColor">
-            <h4 class="title">Sign up</h4>
-          </md-card-header>
+    <form novalidate @submit.prevent="validateUser">
+      <div class="md-layout md-alignment-center">
+        <div class="md-layout-item md-medium-size-100 md-size-50">
+          <md-card>
+            <md-card-header :data-background-color="dataBackgroundColor">
+              <h4 class="title">Sign up</h4>
+            </md-card-header>
 
-          <md-card-content>
-            <div class="md-layout">
-              <div class="md-layout-item md-small-size-100 md-size-50">
-                <md-field>
-                  <label>Username</label>
-                  <md-input v-model="username" type="text"></md-input>
-                </md-field>
+            <md-card-content>
+              <div class="md-layout">
+                <div class="md-layout-item md-small-size-100 md-size-50">
+                  <md-field :class="getValidationClass('username')">
+                    <label for="username">Username</label>
+                    <md-input
+                      type="text"
+                      name="username"
+                      id="username"
+                      v-model="form.username"
+                      :disabled="sending"
+                    ></md-input>
+                    <span class="md-error" v-if="!$v.form.email.required">The username is required</span>
+                    <span class="md-error" v-else-if="!$v.form.email.minLength">Invalid username</span>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-small-size-100 md-size-50">
+                  <md-field :class="getValidationClass('email')">
+                    <label for="email">Email Address</label>
+                    <md-input
+                      type="email"
+                      name="email"
+                      id="email"
+                      v-model="form.email"
+                      :disabled="sending"
+                    ></md-input>
+                    <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+                    <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-small-size-100 md-size-50">
+                  <md-field :class="getValidationClass('password')">
+                    <label>Password</label>
+                    <md-input
+                      type="password"
+                      name="password"
+                      id="password"
+                      v-model="form.password"
+                      :disabled="sending"
+                    ></md-input>
+                    <span class="md-error" v-if="!$v.form.password.required">The password is required</span>
+                    <span class="md-error" v-else-if="!$v.form.password.minLength">Invalid password</span>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-size-100 text-center">
+                  <span class="text-error">{{ error }}</span>
+                </div>
+                <div class="md-layout-item md-size-100 text-center">
+                  <md-button
+                    type="submit"
+                    class="md-raised md-success"
+                    :disabled="sending"
+                  >
+                    Register
+                  </md-button>
+                </div>
               </div>
-              <div class="md-layout-item md-small-size-100 md-size-50">
-                <md-field>
-                  <label>Email Address</label>
-                  <md-input v-model="email" type="email"></md-input>
-                </md-field>
-              </div>
-              <div class="md-layout-item md-small-size-100 md-size-50">
-                <md-field>
-                  <label>Password</label>
-                  <md-input v-model="password" type="password"></md-input>
-                </md-field>
-              </div>
-              <div class="md-layout-item md-size-100 text-center">
-                <span class="text-error">{{ error }}</span>
-              </div>
-              <div class="md-layout-item md-size-100 text-center">
-                <md-button class="md-raised md-success" @click="register"
-                  >Register</md-button
-                >
-              </div>
-            </div>
-          </md-card-content>
-        </md-card>
+            </md-card-content>
+          </md-card>
+        </div>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <script>
 import { UsersService } from "@/services/users.service";
+import { validationMixin } from 'vuelidate';
+import {
+  required,
+  email,
+  minLength,
+  // maxLength
+} from 'vuelidate/lib/validators';
 
 export default {
   name: "LoginUser",
+  mixins: [validationMixin],
   data() {
     return {
       dataBackgroundColor: "green",
-      username: "kot",
-      email: "kot@gmail.com",
-      password: "kot",
+      form: {
+        username: "",
+        email: "",
+        password: "",
+      },
+      sending: false,
       error: ""
     };
   },
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(8)
+      },
+      username: {
+        required,
+        minLength: minLength(3)
+      }
+    }
+  },
   methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
+        }
+      }
+    },
+
     async register() {
       const data = {
-        username: this.username,
-        email: this.email,
-        password: this.password
+        username: this.form.username,
+        email: this.form.email,
+        password: this.form.password
       };
 
       try {
+        this.sending = true;
         await UsersService.create(data);
         this.error = "";
         this.notifyVue({
@@ -74,8 +141,18 @@ export default {
           msg: "Account has been created"
         });
       } catch (error) {
+        this.sending = false;
         console.log(error.message);
         this.error = error.status === 404 ? "Bad request" : error.message;
+      }
+    },
+
+    
+    validateUser () {
+      this.$v.$touch()
+
+      if (!this.$v.$invalid) {
+        this.register()
       }
     },
 
@@ -94,3 +171,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.md-field.md-theme-default.md-invalid label {
+  color: #ff1744 !important;
+}
+</style>
