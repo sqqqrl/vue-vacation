ca3f23d512b005946f6aa30234c4e8808bc3da44
Konstantin Trukhanov <kostya738961@gmail.com>
2021-04-22 12:59:09 +0300

    assert password

--- server/src/rootcommon/checkPassword.js
+++ server/src/rootcommon/checkPassword.js
@@ -1,7 +1,10 @@
 const bcrypt = require('bcryptjs')
-const { errorCodes, AppError } = require('server-core')
+const { errorCodes, AppError, assert } = require('server-core')
 
 function checkPassword (password, hash) {
+  assert.string(password, { notEmpty: true })
+  assert.string(hash, { notEmpty: true })
+
   return new Promise((resolve, reject) => {
     bcrypt.compare(password, hash, (error, result) => {
       if (error) return reject(error)
--- server/src/rootcommon/jwt.js
+++ server/src/rootcommon/jwt.js
@@ -1,10 +1,17 @@
 const jwt = require('jsonwebtoken')
+const { errorCodes, AppError, assert } = require('server-core')
 
 function jwtVerify (token, SECRET) {
+  assert.string(token, { notEmpty: true })
+  assert.string(SECRET, { notEmpty: true })
+
   return new Promise((resolve, reject) => {
     jwt.verify(token, SECRET, (error, decoded) => {
       if (error) {
-        return reject({message: error.message })
+        if (error.name === 'TokenExpiredError') {
+          return reject(new AppError({ ...errorCodes.TOKEN_EXPIRED }))
+        }
+        return reject(new AppError({ ...errorCodes.TOKEN_VERIFY, message: error.message }))
       }
 
       return resolve(decoded)
@@ -13,9 +20,13 @@ function jwtVerify (token, SECRET) {
 }
 
 function jwtSign(payload, SECRET, options) {
+  assert.object(payload, { required: true })
+  assert.string(SECRET, { notEmpty: true })
+  assert.object(options, { notEmpty: true })
+
   return new Promise((resolve, reject) => {
     jwt.sign(payload, SECRET, options, (error, token) => {
-      if (error) return reject({ message: error.message })
+      if (error) return reject(new AppError({ ...errorCodes.TOKEN_NOT_SIGNED, message: error.message }))
       return resolve(token)
     })
   })
