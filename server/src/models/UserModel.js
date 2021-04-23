5a836088438db397fcae983ce691f63fdd16450f
Konstantin Trukhanov <kostya738961@gmail.com>
2021-04-23 11:38:51 +0300

    added validation rules

--- server/src/models/UserModel.js
+++ server/src/models/UserModel.js
@@ -1,17 +1,42 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    company: String,
-    username: String,
-    email: String,
-    password: String,
-    firstname: String,
-    lastname: String,
-    adress: String,
-    phone: Number
-  })
-);
+const schema = new mongoose.Schema({
+  company: {
+    type: String,
+    maxlength: 128
+  },
+  email: {
+    type: String,
+    match: /^\S+@\S+\.\S+$/,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    maxlength: 128
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 128
+  },
+  firstname: {
+    type: String,
+    maxlength: 128,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    maxlength: 128,
+    trim: true
+  },
+  phone: {
+    type: Number,
+    maxlength: 12,
+    trim: true
+  }
+})
+
+const User = mongoose.model("User", schema);
 
 module.exports = User;
